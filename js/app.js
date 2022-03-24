e63d3afafb4ff31d0c3cf5bfbdc7b2ad97922de3
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-03-24 13:13:59 -0700

    adds if statements and button checks for answers

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@ const $question = $('#question');
 const $category = $('#category');
 const $type = $('#type');
 const $difficulty = $('#difficulty');
-const $button = $('button');
-const $choice1 = $('')
-const array = [];
-
+const $submit = $('button');
+// const $input = $('input[name="choice"]');
 
 const getQuiz = () => {
     let data = $.ajax(url);
@@ -17,16 +15,12 @@ function randomizeAnswers (array) {
     for (let i = array.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
-    console.log(i);
-    console.log(j);
-    console.log(array)
         }
-    }
+    } 
 
 getQuiz().then(data => {
     let question = data.results[0].question;
     $question.append(question)
-    console.log(data)
 
     let category = data.results[0].category;
     $category.append(category)
@@ -38,12 +32,25 @@ getQuiz().then(data => {
     $difficulty.append(difficulty)
 
     const possibleAnswers = [...data.results[0].incorrect_answers, data.results[0].correct_answer];
+
     randomizeAnswers(possibleAnswers);
 
     for (let i = 0; i < 4; i++) {
         let index = i + 1;
-
+        const $choice = $(`#choice${index}label`)
+        $choice.html(possibleAnswers[i]);
+        $choice.val(possibleAnswers[i]);
     }
+
+    $submit.on('click', (e) => {
+        e.preventDefault();
+        let correctAnswer = data.results[0].correct_answer;
+        let incorrectAnswers = data.results[0].incorrect_answers;
+        document.querySelectorAll('input[name="choice"]').forEach((el) => {
+            if(el.checked) {
+            }
+        })
+    })
 
 })
 
