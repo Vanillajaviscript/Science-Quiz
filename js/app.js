59e277656580b69017bb80ae570b94946a8b5b98
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-03-24 10:27:08 -0700

    adds html labels/inputs and js loops

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
-const url = "https://opentdb.com/api.php?amount=10"
+const url = "https://opentdb.com/api.php?amount=15&type=multiple";
 const $question = $('#question');
 const $category = $('#category');
-const $type = $('#type')
+const $type = $('#type');
+const $difficulty = $('#difficulty');
+const $button = $('button');
+const $choice1 = $('')
+const array = [];
 
 
-async function getQuestions() {
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(url)
-    return data
+const getQuiz = () => {
+    let data = $.ajax(url);
+    return data;
 }
 
-getQuestions().then(data => {
+function randomizeAnswers (array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+    console.log(i);
+    console.log(j);
+    console.log(array)
+        }
+    }
+
+getQuiz().then(data => {
     let question = data.results[0].question;
-    $question.html = data.results[0].question;
     $question.append(question)
     console.log(data)
 
     let category = data.results[0].category;
-    $category.html = data.results[0].category;
     $category.append(category)
 
-    let type = data.results[0].type;
-    $type.html = data.results[0].type;
+    let type = data.results[0].type + " choice";
     $type.append(type)
+
+    let difficulty = data.results[0].difficulty;
+    $difficulty.append(difficulty)
+
+    const possibleAnswers = [...data.results[0].incorrect_answers, data.results[0].correct_answer];
+    randomizeAnswers(possibleAnswers);
+
+    for (let i = 0; i < 4; i++) {
+        let index = i + 1;
+
+    }
+
 })
 
