0f055ec4d8bc278c59a7257ec7dcf8d228e3493e
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-03-24 18:46:50 -0700

    adds js reset button and automatic reload when correct answer is chosen

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-const url = "https://opentdb.com/api.php?amount=15&type=multiple";
+//Declare variables to the DOM;
+//Declare arrow function and within pull API data using AJAX
+//Call function and declare variables pulling necessary data from API
+//Apply the pulled data from API and turn into array
+//Create a function to randomize possible answers
+
+const url = "https://opentdb.com/api.php?amount=10&category=17&type=multiple"
 const $question = $('#question');
 const $category = $('#category');
 const $type = $('#type');
 const $difficulty = $('#difficulty');
 const $submit = $('button');
-const $resetButton = $(':reset')
+const $reset = $(':reset')
 
 const getQuiz = () => {
     let data = $.ajax(url);
@@ -28,18 +34,19 @@ getQuiz().then(data => {
         $(`#answer${index}`).val(possibleAnswers[i]);
         $(`#answer${index}label`).html(possibleAnswers[i]);
     }
-
+    //API data to pull 
     let question = data.results[0].question;
         $question.append(question);
-
     let category = data.results[0].category;
         $category.append(category);
-
     let type = data.results[0].type + " choice";
         $type.append(type);
-
     let difficulty = data.results[0].difficulty;
         $difficulty.append(difficulty);
+
+    $reset.on('click', () => {
+        location.reload();
+    })
 
     $submit.on('click', (e) => {
         e.preventDefault();
@@ -48,6 +55,7 @@ getQuiz().then(data => {
             if(el.checked) {
                 if(el.value === correctAnswer) {
                     alert('Correct!')
+                    location.reload();
                 } else {
                     alert('Wrong answer, try again!')
                 }
